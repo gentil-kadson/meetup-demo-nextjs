6ec05f5550a22f375ae9eb0eb5cd0c7e2b1eb481
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2022-06-18 12:59:13 -0300

    studied getServerSideProps and getStaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,14 +12,39 @@ const DUMMY_MEETUUPS = [
   {
     id: "m2",
     title: "A Second Meetup",
-    image: "https://thenexus.one/storage/2022/04/Star-Wars-Coruscant.jpg",
+    image: "http://comoprojetar.com.br/wp-content/uploads/2017/08/http-static.srcdn_.comwp-contentuploads201611starwars-tatooine-twin-suns.jpg",
     address: "Some address 5, 6789 Some City",
     description: "This is a second meetup!",
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// you should only use getServerSideProps when you need access to the request body and need to reload the page instantly every second, because then it'll be accurate when handling a lot of requests.
+
+// export async function getServerSideProps(context) {
+//   const req = context.req; // can work with req.body as in Express, for example.
+//   const res = context.res;
+
+//   // fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUUPS
+//     }
+//     // validade won't be available here because Next'll already fetch the data when the server receives a new request.
+//   }
+// }
+
+export async function getStaticProps() {
+  // fetch data from API
+  return {
+    props: {
+      meetups: DUMMY_MEETUUPS
+    },
+    revalidate: 1
+  }
 }
 
 export default HomePage;
